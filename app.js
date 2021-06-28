bcd3a33c217c4938c2db694936d54bd382d77023
Spectromon <tommycarter52@gmail.com>
2021-06-28 11:22:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
 // clearInterval() #Stop above
 
 const port = process.env.PORT || 3000
-const express = require('express');
-const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-  });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-app.listen(3000, () => {
-    console.log('Server listening on http://localhost:3000');
-  });
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
