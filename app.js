f359c515019887595522d5ec64d06f1a883d59ce
Spectromon <tommycarter52@gmail.com>
2021-06-28 13:10:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const env = process.env.NODE_ENV || 'development'
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
   
 // Getting Request
 app.get('/', (req, res) => {
