aa1d4f657182da660a8fa57dc4a881bd70da57b4
Spectromon <tommycarter52@gmail.com>
2021-06-28 11:48:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,18 @@
 //clearTimeout() #Stop above
 // setInterval() #Call a function repeatedly after a delay
 // clearInterval() #Stop above
-// Importing express module
 const env = process.env.NODE_ENV || 'development'
 const express = require('express');
 const app = express();
   
 // Getting Request
 app.get('/', (req, res) => {
-  
-    // Sending the response
-    res.send('Hello World!')
-     
-    // Ending the response 
-    res.end()
-})
+    res.sendFile('views/index.html', {root: __dirname })
+});
   
 // Establishing the port 
 const PORT = process.env.PORT ||5000;
   
 // Executing the sever on given port number
 app.listen(PORT, console.log(
-  `Server started on port ${PORT}`));
+  `Server started on http://localhost:${PORT}`));
