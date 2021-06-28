90a8ec8654711b35b4c7df5dda990f98076bfc39
Spectromon <tommycarter52@gmail.com>
2021-06-28 16:29:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,6 @@
 //clearTimeout() #Stop above
 // setInterval() #Call a function repeatedly after a delay
 // clearInterval() #Stop above
-//setTimeout() # Call a function after a delay
-//clearTimeout() #Stop above
-// setInterval() #Call a function repeatedly after a delay
-// clearInterval() #Stop above
 const env = process.env.NODE_ENV || 'development'
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -23,6 +19,17 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
   
 // Getting Request
 app.get('/', (req, res) => {
@@ -31,12 +38,13 @@ app.get('/', (req, res) => {
 
 //This works. Requires urlencodedParser however does send an email to the specific inputted email address.
 app.post('/', urlencodedParser, (req,res) =>{
-  console.log(req.body.email) //you will get your data in this as object.
+  console.log(req.body.email) //you will get your data in this as object
+  u_name = makeid(5)
   var mailOptions = {
     from: 'rgu20daudissertation@gmail.com',
     to: req.body.email,
     subject: 'Sending Email using Node.js',
-    text: 'Test email for sending data to this account'
+    text: 'Your username is ' + u_name
   };
 
   transporter.sendMail(mailOptions);
