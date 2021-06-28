33389fdbbeaa5442d4f6109c57f5801e1279acf7
Spectromon <tommycarter52@gmail.com>
2021-06-28 16:18:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,47 @@
 //clearTimeout() #Stop above
 // setInterval() #Call a function repeatedly after a delay
 // clearInterval() #Stop above
+//setTimeout() # Call a function after a delay
+//clearTimeout() #Stop above
+// setInterval() #Call a function repeatedly after a delay
+// clearInterval() #Stop above
 const env = process.env.NODE_ENV || 'development'
 const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const nodemailer = require('nodemailer');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express();
 app.use(express.static(__dirname));
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'rgu20daudissertation@gmail.com',
+    pass: 'yMspnGe#Q!a4t5g9'
+  }
+});
+
   
 // Getting Request
 app.get('/', (req, res) => {
-    res.sendFile('rubick.html', {root: __dirname })
+  res.sendFile('signup.html', {root: __dirname })
 });
+
+//This works. Requires urlencodedParser however does send an email to the specific inputted email address.
+app.post('/', urlencodedParser, (req,res) =>{
+  console.log(req.body.email) //you will get your data in this as object.
+  var mailOptions = {
+    from: 'rgu20daudissertation@gmail.com',
+    to: req.body.email,
+    subject: 'Sending Email using Node.js',
+    text: 'Test email for sending data to this account'
+  };
+
+  transporter.sendMail(mailOptions);
+
+  res.sendFile('signup.html', {root: __dirname })
+})
   
 // Establishing the port 
 const PORT = process.env.PORT ||5000;
