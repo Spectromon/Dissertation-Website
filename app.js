4c10613d0e7dd6af703e2e8ad43e9c926d87eb78
Spectromon <tommycarter52@gmail.com>
2021-06-29 13:07:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.post('/login',urlencodedParser, (req,res) =>{
   u_name = req.body.username
   p_word = req.body.password
   var sql = {text: 'SELECT * FROM u_info where u_name = $1 and p_word = $2;', values: [u_name, p_word]}
-  checker = client.query(sql, function (err, res){
+  client.query(sql, checker = (err, res) =>{
     if(err){
       console.log(err)
     }
